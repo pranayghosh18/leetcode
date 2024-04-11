5ff74f5b0c2badd3d60a58717abce5b6d7ef1fcc
pranayghosh18 <67771835+pranayghosh18@users.noreply.github.com>
2024-04-11 14:03:42 +0530

    Time: 4 ms (97.62%) | Memory: 10.3 MB (18.33%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string removeKdigits(string s, int k) {
+        // if(s.size() >= 10000) return "";
+        stack<char> st;
+        int i(0);
+        while(i<s.size()){
+            if(st.empty() || s[i] >= st.top()){
+                st.push(s[i]);
+                i++;
+            }
+            else{
+                while( !st.empty() && k>0 && st.top() > s[i]){
+                    st.pop();
+                    k--;
+                }
+                st.push(s[i]);
+                i++;
+            }
+        }
+        string r;
+        // filling res str
+        while(!st.empty()){
+            r.push_back(st.top());
+            st.pop();
+        }
+        // cout << "27 line : " << r << endl;
+        // cout << "r back : " << r.back() << endl;
+        // delete right 0s
+        while(r.size() > 0 && r.back()=='0') r.pop_back();
+        // cout << "30 line : " << r << endl;
+
+        // if(r.size() > 0) 
+        reverse(r.begin(),r.end());
+        // str is not like 123456
+        while(k>0 && r.size() > 0){
+            r.pop_back();
+            k--;
+        }
+        if(r.size()==0) return "0";
+        return r;
+    }
+};
