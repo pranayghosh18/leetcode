ad115922057644c43c1002317fdf02340b08f878
pranayghosh18 <67771835+pranayghosh18@users.noreply.github.com>
2024-08-18 17:46:26 +0530

    Time: 113 ms (94.89%) | Memory: 82.4 MB (62.98%) - LeetSync

--- 883-car-fleet/car-fleet.cpp
+++ 883-car-fleet/car-fleet.cpp
@@ -10,12 +10,14 @@ int carFleet(int target, vector<int>& position, vector<int>& speed) {
     for(int i=posSpeed.size()-1; i>=0; i--){
         int distance = target - posSpeed[i].first;
         int velocity = posSpeed[i].second;
-
+        // this cars time > prev some cars time
+        // ie this car come as a isolated one
         if((ll)distance*(ll)prevSpeed > (ll)prevDist*(ll)velocity){
             count++;
             prevDist = distance;
             prevSpeed = velocity;
         }
+        // else, current car getting merged with the fleet
     }     
     return count;   
 }
