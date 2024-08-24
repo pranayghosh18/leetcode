813369408639803b4afa73cb9deb5a72dc2f099c
pranayghosh18 <67771835+pranayghosh18@users.noreply.github.com>
2024-08-24 23:07:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (51.34%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,66 +1,20 @@
-#define pii pair<int,int>
-#define db double
-#define ll long long
-
-#define vi vector<int>
-#define vb vector<bool>
-#define pb push_back
-#define vvi vector<vector<int>>
-#define um unordered_map
-#define us unordered_set
-
-#define qi queue<int>
-#define sz size()
-#define em empty()
-#define fr front()
-#define po pop()
-#define pu push
-#define in insert
-
-#define ff first
-#define ss second
-#define rr return 
-
-#define rep(i,b,e) for(int i=b; i<e; i++)
-#define fa(i,v) for(auto& i : v)
-
-#define xsort(v) sort(v.begin(),v.end())
-#define elif else if
-#define MOD 1000000007
-
 class Solution {
 public:
-    ll res;
-    void dfs(){}
-    int numDecodings(string s) {
-        vi ddp(s.sz,0);
-        if(s[0]=='0') rr 0;
-        ddp[0]=1;
-        if(s.sz==1) return ddp[0];
-        
-        string frst2 = s.substr(0,2);
-        if(s[1]=='0'){
-            ddp[1] = frst2 <= "26" ? 1 : 0;
+int numDecodings(string s) {
+    if(s[0] == '0') return 0;
+    vector<int> dp(s.size(), 0);
+    dp[0] = 1;
+    for(int i=1; i<s.size(); i++){
+        string lastTwos = s.substr(i-1, 2);
+        // if(lastTwos >= "26") return 0;
+        // validity
+        if(s[i-1] > '0' && lastTwos <= "26") {
+            // dp[i] += 1;
+            if(i-2 >= 0) dp[i] += dp[i-2];
+            else dp[i] += 1;
         }
-        else{
-            ddp[1] = frst2 <= "26" ? 2 : 1;
-        }
-        rep(i,2,s.sz){
-            string chunk = s.substr(i-1,2);
-            if(s[i]=='0'){
-                //if(s[i-1]=='0') ddp[i] = 0;
-                //else
-                    ddp[i] = s[i-1] != '0' && chunk <= "26" ? ddp[i-2] : 0;
-            }
-            else{
-                // if(s[i-1]=='0') ddp[i] = ddp[i-1];
-                // else
-                    ddp[i] = s[i-1] != '0' && chunk <= "26" ? ddp[i-2]+ddp[i-1] : ddp[i-1];
-            }
-        }
-        return ddp[s.sz-1];
+        if(s[i] != '0') dp[i] += dp[i-1];
     }
+    return dp.back();
+}
 };
-
-
-
