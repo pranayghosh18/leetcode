fd3150e68e5c81dae8339e05ed48a1cf02f80e0b
pranayghosh18 <67771835+pranayghosh18@users.noreply.github.com>
2025-06-13 00:52:04 +0530

    Time: 3 ms (15.72%) | Memory: 17.6 MB (84.79%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
 int maxProduct(vector<int>& nums) {
-    vector<int> maxProduct(nums.size()), minProducts(nums.size());
+    int maxProduct(nums[0]), minProduct(nums[0]);
     int res(*max_element(nums.begin(), nums.end()));
-    maxProduct[0] = minProducts[0] = nums[0];
+ 
     for(int i=1; i<nums.size(); i++){
-        if(nums[i] == 0) maxProduct[i] = minProducts[i] = 1;
+        if(nums[i] == 0) maxProduct = minProduct = 1;
         else{
-            maxProduct[i] = max({maxProduct[i-1]*nums[i], minProducts[i-1]*nums[i], nums[i]});
-            minProducts[i] = min({maxProduct[i-1]*nums[i], minProducts[i-1]*nums[i], nums[i]});
-            res = max(res, maxProduct[i]);
+            int temp = maxProduct;
+            maxProduct = max({maxProduct*nums[i], minProduct*nums[i], nums[i]});
+            minProduct = min({temp*nums[i], minProduct*nums[i], nums[i]});
+            res = max(res, maxProduct);
         }
     }
     return res;
