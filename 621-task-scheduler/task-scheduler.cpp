f9edce2d074735a3bd592bda49b9d9ffad24d00b
pranayghosh18 <67771835+pranayghosh18@users.noreply.github.com>
2025-06-16 16:27:24 +0530

    Time: 3 ms (74.19%) | Memory: 38.4 MB (69.83%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
-    int leastInterval(vector<char>& tasks, int n) {
-        if(n==0) return tasks.size();
-        int a[26] = {0};
-        for(char c : tasks) a[c-'A']++;
-        priority_queue<int> mxh;
-        for(int i : a) if(i > 0) mxh.push(i);
-        int tq = n+1;
-        int period(0);
-       while(!mxh.empty()){
-            // pick n tasks and reduce there time quantum by 1
-            multiset<int> next_cycle;
-            int executed(0);
-            for(int i=1; i<=tq && !mxh.empty(); i++) {
-                if(mxh.top()-1 > 0) next_cycle.insert( mxh.top()-1 );
-                mxh.pop();
-                executed++;
-                }
-            if(next_cycle.empty() && mxh.empty()){
-                period += executed;
-                return period;
-            }
-            period += max(executed, tq);
-            for(int i : next_cycle) mxh.push(i);
-        }
-        return period;
-    }
+ 
+int leastInterval(vector<char>& tasks, int n) {
+    unordered_map<char, int> freq;
+    int maxFreq(0);
+    for(char t : tasks) freq[t]++, maxFreq=max(maxFreq, freq[t]);
+    int ans((maxFreq-1)*(n+1));    
+    for(auto t : freq) if(t.second == maxFreq) ans++;
+    return max(ans, int(tasks.size()));
+}
 };
