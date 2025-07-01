420097e55bf356a536f742681b654999db9c29f5
pranayghosh18 <67771835+pranayghosh18@users.noreply.github.com>
2025-07-01 23:19:58 +0530

    Time: 1 ms (87.19%) | Memory: 42.3 MB (26.45%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.java
@@ -1,19 +1,25 @@
 class Solution {
     public static int possibleStringCount(String word) {
-        ArrayList<Integer> count = new ArrayList<>(word.length());
+        // ArrayList<Integer> count = new ArrayList<>(word.length());
+        int counter[] = new int[word.length()];
         // System.out.println(count);
-        for(int i=0; i<word.length(); i++) count.add(0);
+        // for(int i=0; i<word.length(); i++) count.add(0);
         // System.out.println(count);
 
         // count.add(0);
         for(int i=1; i<word.length(); i++){
             if(word.charAt(i) == word.charAt(i-1)) {
-                count.set(i, 1 + count.get(i-1));
+                // count.set(i, 1 + count.get(i-1));
+                counter[i] = counter[i-1] + 1;
             }
         }
+        // System.out.println(counter);
         // System.out.println(count);
-        int ans = 0;
-        for(int c : count) if(c>0) ans ++;
-        return ans+1;
+        // int ans = 0;
+        int res = 0;
+        for(int t : counter) if(t > 0) res++;
+        // for(int c : count) if(c>0) ans++;
+        // return ans+1;
+        return res+1;
     }
 }
