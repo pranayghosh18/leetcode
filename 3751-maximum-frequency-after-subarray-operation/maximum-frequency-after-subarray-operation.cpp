a5f507528004943cc27df357680e6391f26652dd
pranayghosh18 <67771835+pranayghosh18@users.noreply.github.com>
2025-07-12 13:39:16 +0530

    Time: 97 ms (11.94%) | Memory: 186.8 MB (7.44%) - LeetSync

---
+++ 3751-maximum-frequency-after-subarray-operation/maximum-frequency-after-subarray-operation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+int onesInList(vector<int>& lis){
+    // for(int i: lis) cout << i << "\t";
+    // cout<< endl;
+    
+    int sum(0), maxSum(0);
+    for(int i: lis){
+        sum = max(i, sum+i);
+        maxSum = max(maxSum, sum);
+    }
+    // cout << maxSum;
+    // cout<< endl;
+    return maxSum;
+}
+
+int maxFrequency(vector<int>& nums, int k) {
+    int kFreq(0);
+    unordered_set<int> uniques;
+    for(int i: nums) {
+        uniques.insert(i);
+        if(i == k) kFreq++;
+    }
+    int res(kFreq);
+    for(int i : uniques){
+        vector<int> clone(nums.size());
+        for(int p=0; p<nums.size(); p++){
+            if(nums[p] == k) clone[p] = -1;
+            else clone[p] = nums[p] == i ? 1 : 0;
+        }
+        res = max(res, kFreq + onesInList(clone));
+    }
+    return res;
+}
+};
