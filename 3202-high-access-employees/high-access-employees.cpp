7cd8474eb1ca55d37db86432db22cb00bfe60898
pranayghosh18 <67771835+pranayghosh18@users.noreply.github.com>
2025-07-17 21:30:20 +0530

    Time: 47 ms (91.83%) | Memory: 53.3 MB (37.35%) - LeetSync

--- 3202-high-access-employees/high-access-employees.cpp
+++ 3202-high-access-employees/high-access-employees.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
+vector<string> findHighAccessEmployees(vector<vector<string>>& access_times) {
     unordered_map<string, vector<int>> access;
     for(auto rec : access_times){
         string emp = rec[0];
         for(int i=1; i<rec.size(); i++) access[emp].push_back(stoi(rec[i]));
     }        
     vector<string> ans;
-    for(auto rec : access) {
-        string emp = rec.first;
-        sort(rec.second.begin(), rec.second.end());
-        for(int r=0, f=0; r<rec.second.size(); r++){
+    for(auto& [rec, list] : access) {
+        string emp = rec; // .first;
+        sort(list.begin(), list.end());
+        for(int r=0, f=0; r<list.size(); r++){
             if(r-f >= 2) {
                 // checking the fixed window within a hour
-                if(rec.second[r] - rec.second[f] < 100){
+                if(list[r] - list[f] < 100){
                     ans.push_back(emp);
                     break;
                 }
